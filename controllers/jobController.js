ce304788e80ac9e5b5c22cec56d843305412cfbd
UzomaAkpala <francisakpala@yahoo.com>
2023-08-14 15:49:07 +0100

    - Amin User

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,5 @@
 import Job from "../models/jobModel.js";
 import { StatusCodes } from "http-status-codes";
-import { NotFoundError } from "../errors/customErrors.js";
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -15,7 +14,6 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ job });
 };
 
@@ -24,7 +22,6 @@ export const updateJob = async (req, res) => {
   const updateJob = await Job.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-  if (!updateJob) throw new NotFoundError(`no job with id ${id}`);
 
   res.status(StatusCodes.OK).json({ msg: "job modified", job: updateJob });
 };
@@ -32,7 +29,6 @@ export const updateJob = async (req, res) => {
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findByIdAndDelete(id);
-  if (!removedJob) throw new NotFoundError(`no job with id ${id}`);
 
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: removedJob });
 };
