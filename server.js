92d1771b895c272a020cbedfaa1ecbf5490dee45
UzomaAkpala <francisakpala@yahoo.com>
2023-08-15 13:42:13 +0100

    data fetching in node

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 
+// different methods for fetching data in node
+// number 1
+const getData = async () => {
+  const response = await fetch(
+    "https://www.course-api.com/react-useReducer-cart-project"
+  );
+  const cartData = await response.json();
+  console.log(cartData);
+};
+getData();
+
+// number 2
+fetch("https://www.course-api.com/react-useReducer-cart-project")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+//number 3
+
+try {
+  const response = await fetch(
+    "https://www.course-api.com/react-useReducer-cart-project"
+  );
+  const cartData = await response.json();
+  console.log(cartData);
+} catch (error) {
+  console.log(error);
+}
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
