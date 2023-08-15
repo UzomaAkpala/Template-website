48a8facb3f3a27224f1bbbc87fd77d4bd7f3b816
UzomaAkpala <francisakpala@yahoo.com>
2023-08-15 14:00:19 +0100

    CRUD - get hardCoded jobs data from server

--- server.js
+++ server.js
@@ -3,34 +3,12 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import { nanoid } from "nanoid";
 
-// different methods for fetching data in node
-// number 1
-const getData = async () => {
-  const response = await fetch(
-    "https://www.course-api.com/react-useReducer-cart-project"
-  );
-  const cartData = await response.json();
-  console.log(cartData);
-};
-getData();
-
-// number 2
-fetch("https://www.course-api.com/react-useReducer-cart-project")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-
-//number 3
-
-try {
-  const response = await fetch(
-    "https://www.course-api.com/react-useReducer-cart-project"
-  );
-  const cartData = await response.json();
-  console.log(cartData);
-} catch (error) {
-  console.log(error);
-}
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -46,6 +24,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
