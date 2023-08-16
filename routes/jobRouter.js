92e2968728b5c00d265e592f6332ec25881a8aa6
UzomaAkpala <francisakpala@yahoo.com>
2023-08-16 13:56:07 +0100

    Validate Job Inputs

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -8,10 +8,15 @@ import {
   updateJob,
   deleteJob,
 } from "../controllers/jobController.js";
+import { validateJobInput } from "../middleware/validationMiddleware.js";
 
 //router.get('/', getAllJobs)
 
-router.route("/").get(getAllJobs).post(createJob);
-router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
+router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route("/:id")
+  .get(getJob)
+  .patch(validateJobInput, updateJob)
+  .delete(deleteJob);
 
 export default router;
