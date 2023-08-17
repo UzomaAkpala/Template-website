95fc0de3cef315654c601c0c75fd8f4c79ac6ae3
UzomaAkpala <francisakpala@yahoo.com>
2023-08-17 13:53:49 +0100

    Logout Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,9 @@ export const login = async (req, res) => {
   if (!isPasswordCorrect) throw new UnauthenticatedError("invalid credentials");
 
   const token = createJWT({ userId: user._id, role: user.role });
+
   const oneDay = 1000 * 60 * 60 * 24;
+
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
@@ -33,4 +35,12 @@ export const login = async (req, res) => {
   });
 
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
