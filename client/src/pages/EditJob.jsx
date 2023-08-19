a253844ffeb73bc8d12c7a3572bf96bcf46f73ae
UzomaAkpala <francisakpala@yahoo.com>
2023-08-19 22:05:08 +0100

    Edit Job Loader

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -7,8 +7,13 @@ import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
 
 export const loader = async ({ params }) => {
-  console.log(params);
-  return null;
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return redirect("/dashboard/all-jobs");
+  }
 };
 
 export const action = async () => {
@@ -16,6 +21,8 @@ export const action = async () => {
 };
 
 const EditJob = () => {
+  const { job } = useLoaderData();
+  console.log(job);
   return <h1>EditJob Page</h1>;
 };
 export default EditJob;
