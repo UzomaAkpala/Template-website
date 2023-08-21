c6110d73188bded138e160379889dffb1138ffa8
UzomaAkpala <francisakpala@yahoo.com>
2023-08-21 21:01:10 +0100

    Restrict Access to test user

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -12,14 +12,18 @@ import {
   validateJobInput,
   validateIdParam,
 } from "../middleware/validationMiddleware.js";
+import { checkForTestUser } from "../middleware/authMiddleware.js";
 
 //router.get('/', getAllJobs)
 
-router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route("/")
+  .get(getAllJobs)
+  .post(checkForTestUser, validateJobInput, createJob);
 router
   .route("/:id")
   .get(validateIdParam, getJob)
-  .patch(validateJobInput, updateJob)
-  .delete(validateIdParam, deleteJob);
+  .patch(checkForTestUser, validateJobInput, updateJob)
+  .delete(checkForTestUser, validateIdParam, deleteJob);
 
 export default router;
