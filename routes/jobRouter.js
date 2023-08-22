5866dc6f80716f5d184962d84eeb821831414b81
UzomaAkpala <francisakpala@yahoo.com>
2023-08-22 10:32:05 +0100

    Stats group setup

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -7,6 +7,7 @@ import {
   createJob,
   updateJob,
   deleteJob,
+  showStats,
 } from "../controllers/jobController.js";
 import {
   validateJobInput,
@@ -20,6 +21,9 @@ router
   .route("/")
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
+
+router.route("/stats").get(showStats);
+
 router
   .route("/:id")
   .get(validateIdParam, getJob)
