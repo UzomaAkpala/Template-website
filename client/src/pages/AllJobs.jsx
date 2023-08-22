2dc8319f0ad69191bada9f4f3f5e61e75b079062
UzomaAkpala <francisakpala@yahoo.com>
2023-08-22 17:56:09 +0100

    controlled Inputs

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -3,30 +3,37 @@ import { JobsContainer, SearchContainer } from "../components";
 import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 import { useContext, createContext } from "react";
-
+const AllJobsContext = createContext();
 export const loader = async ({ request }) => {
-  console.log(request.url);
-  const params = Object.fromEntries([
-    ...new URL(request.url).searchParams.entries(),
-  ]);
-  console.log(params);
   try {
-    const { data } = await customFetch.get("/jobs");
-    return { data };
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+
+    const { data } = await customFetch.get("/jobs", {
+      params,
+    });
+
+    return {
+      data,
+      searchValues: { ...params },
+    };
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
+    toast.error(error.response.data.msg);
+    return error;
   }
 };
-const AllJobsContext = createContext();
+
 const AllJobs = () => {
-  const { data } = useLoaderData();
-  console.log(data);
+  const { data, searchValues } = useLoaderData();
+
   return (
-    <AllJobsContext.Provider value={{ data }}>
+    <AllJobsContext.Provider value={{ data, searchValues }}>
       <SearchContainer />
       <JobsContainer />
     </AllJobsContext.Provider>
   );
 };
-export const useAllJobsContext = () => useContext(AllJobsContext);
 export default AllJobs;
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
